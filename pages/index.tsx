7fe3a5fce13d0e442def42182226d486648cb313
lucas barra <lucastbarra@hotmail.com>
2021-11-02 11:05:06 -0300

    recovering token on server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import { FormEvent, useContext, useState } from 'react'
+import { parseCookies } from 'nookies';
+
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
 
@@ -30,3 +32,22 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    }
+  }
+
+
+  return {
+    props: {}
+  }
+}
