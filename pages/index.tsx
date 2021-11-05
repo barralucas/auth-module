f453b1de3abb7873438512e53edb57712f59dcec
lucas barra <lucastbarra@hotmail.com>
2021-11-04 23:52:47 -0300

    validating visite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { GetServerSideProps, NextPage } from 'next'
+import type { NextPage } from 'next'
 import { FormEvent, useContext, useState } from 'react'
-import { parseCookies } from 'nookies';
 
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState('');
@@ -34,20 +34,10 @@ const Home: NextPage = () => {
 export default Home
 
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {
+      users: ['asdasd']
     }
   }
-
-
-  return {
-    props: {}
-  }
-}
+})
